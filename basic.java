1d305e2ffc910fb1e1674dbf9f188f0709adaf29
Pranjal goyal <146417277+Pranjalgoyal24@users.noreply.github.com>
2025-04-09 01:30:27 +0530

    Add files via upload

---
+++ basic.java
@@ -0,0 +1,12 @@
+
+
+public class basic{
+    public static void main(String[] args) {
+        System.out.print ("Om namah Shivay\n");
+        System.out.println("Hello world");
+        int x = 6;
+        
+        System.out.println(x);
+        x=90;
+        System.out.println(x );
+    }}
